f89f535a4252f916c79548b8954230e1dd0b036b
Aaron_Sun <1162129136@qq.com>
2017-04-29 01:07:53 +0800

    Update NN.py

--- Python-Version/Python-Version/NN.py
+++ Python-Version/Python-Version/NN.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
+from sklearn.utils import shuffle
 # First, read data from file and preprocess and other functions.
 def Preprocess(FileName):
 	Data = np.load(FileName)
@@ -22,41 +23,53 @@ def Preprocess(FileName):
 	Temp = kmeans.predict(Temp)
 	X=Temp.reshape(N_MS,N_MBS*N_frequency)
 	#H_SBSr=np.reshape(H_SBS,N_MS*N_frequency,N_SBS)
-	H_SBSr=np.reshape(H_SBS,N_MS*N_frequency,10)
+	H_SBSr=np.reshape(H_SBS,(N_MS*N_frequency,10))
 	#H_SBSr and H_SBS are identical, so why should we add this line?
 	y=np.argmax(np.abs(H_SBS),axis=1)
+	Y_temp=np.zeros((np.shape(y)[0],10))
+	for i in range(np.shape(y)[0]):
+		Y_temp[i][y[i]]=1
+	y=Y_temp
 	return (X,y)
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
     # add one more layer and return the output of this layer
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    biases = tf.Variable(tf.random_normal([out_size]) + 0.1)
     Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    regularizer = tf.nn.l2_loss(Weights)+tf.nn.l2_loss(biases)
     if activation_function is None:
         outputs = Wx_plus_b
-    else:
+    else: 
         outputs = activation_function(Wx_plus_b)
-    return outputs
+    return outputs, regularizer
 # Second, divide the dataset into Train and Test set
-X,y=Preprocess(FileName)#X: 2000*100, y:2000*1
-X_train, X_test, y_train, y_test = train_test_split(
-...     X, y, test_size=0.2, random_state=42)#divide the dataset
-
+X,y=Preprocess('/home/aaron/Desktop/信道预测/Python version/DataCL.npz')#X: 2000*100, y:2000*1
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=42)#divide the dataset
+Total=np.concatenate((X_train,y_train),axis=1)
 #Third, define a function to get minibatch from Train randomly
-pass
-#Fourth, tensorflow code here.
+def next_batch():
+	np.random.shuffle(Total)
+	y=Total[1:101, 100:]
+	X=Total[1:101, 0:100]
+	return X,y
+
+#Fourth, tensorflowde here.
+loss_rate=1.8e-1
+hidden_num=7000
 x = tf.placeholder(tf.float32, shape=[None,np.shape(X_train)[1]])
-y_label = tf.placeholder(tf.float32, shape=[None, 1])
-layer1 = add_layer(x, X_train.shape()[0], 100, activation_function=tf.nn.relu)
-y_result = add_layer(layer1, 100, 1, activation_function=None)
+y_label = tf.placeholder(tf.float32, shape=[None, 10])
+layer1, regularizer_1 = add_layer(x, np.shape(X_train)[1], hidden_num, activation_function=tf.nn.relu)
+#layer2 = add_layer(layer1, hidden_num, 40, activation_function=tf.nn.relu)
+y_result, regularizer_2 = add_layer(layer1, hidden_num, 10, activation_function = None)
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_label, logits=y_result))
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+loss=cross_entropy+loss_rate*(regularizer_1+regularizer_2)
+train_step = tf.train.RMSPropOptimizer(0.005).minimize(loss)
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
-for i in range(18):
-	batch_x=X_train[i*200:i*200+199,:]
-	batch_y=y_train[i*200:i*200+199]
+for _ in range(1000):
+	batch_x, batch_y = next_batch()
 	train_step.run(feed_dict={x: batch_x, y_label: batch_y})
 
 correct_prediction = tf.equal(tf.argmax(y_result, 1), tf.argmax(y_label, 1))
